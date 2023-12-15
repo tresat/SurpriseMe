6f5802454277a7d65fc99eeebef4b137c3529d1c
Tom Tresansky <ttresansky@gradle.com>
2023-12-15 10:43:23 -0500

    Add a new multiplatform library

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+            implementation(libs.kotlin.datetime)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- shared/src/commonMain/kotlin/com/tomtresansky/surpriseme/Greeting.kt
+++ shared/src/commonMain/kotlin/com/tomtresansky/surpriseme/Greeting.kt
@@ -8,5 +8,6 @@ class Greeting {
     fun greet(): List<String> = buildList {
         add(if (Random.nextBoolean()) "Hi!" else "Hello!")
         add("Guess what this is! > ${platform.name.reversed()}!")
+        add(daysPhrase())
     }
 }
